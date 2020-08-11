6bd1ad6b7462b51796debd1fa47d415886184eb6
gala <solankivibhakar82@gmail.com>
2020-08-11 16:59:26 +0530

    added django debugger

--- codingplusplus/codingplusplus/urls.py
+++ codingplusplus/codingplusplus/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -33,3 +34,9 @@ urlpatterns = urlpatterns + [
     # of your site, rather than the site root:
     #    url(r"^pages/", include(wagtail_urls)),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
+
